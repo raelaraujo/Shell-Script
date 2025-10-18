4511d05dca1c3a7eeaa57bf847a64e8fd9ae6e6d
raelaraujo0 <raellyedson1@gmail.com>
2025-10-18 17:27:20 -0300

    check if nmap exists

--- scripts/nmap/nmap.sh
+++ scripts/nmap/nmap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # --------------------------------
 #               Nmap 
 # This shell aims to automate 
@@ -10,3 +12,31 @@ USAGE="Usage: ./nmap.sh <target/example>
 Example: ./nmap.sh example
 Note: run chmod +x nmap.sh before exec
 "
+
+# verify args
+if [ $# -ne 1 ]; 
+then
+  echo "$USAGE"
+  exit 1
+fi
+TARGET="$1"
+
+# verify if exists
+NMAP=/usr/bin/nmap
+
+if [ -x "$NMAP" ];
+then
+    echo "$NMAP exists"
+else
+    echo "Nmap NOT found in /usr/bin/nmap. Installing"
+    sudo apt update && sudo apt install -y nmap
+
+    #verify again
+    if [ -x "$NMAP" ];
+    then 
+        echo "$NMAP available"
+    else
+        echo "Error: nmap isnt available yet"
+        exit 1
+    fi
+fi
