a5706a010f4f7df0e64a579f4e1d38c6bd267302
raelaraujo0 <raellyedson1@gmail.com>
2025-10-20 17:06:51 -0300

    feat: verify in enums e dns

--- scripts/dns/dns.sh
+++ scripts/dns/dns.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # --------------------------------
 #              DNS 
 # This shell aims to automate 
@@ -10,3 +12,12 @@ USAGE="Usage: ./dns.sh <target/example>
 Example: ./dns.sh example.com
 Note: run chmod +x dns.sh before exec
 "
+
+# verify args
+if [ $# -ne 1 ]; 
+then
+    echo "$USAGE"
+    exit 1
+fi
+
+# verify if exists
--- scripts/subdomains/enums.sh
+++ scripts/subdomains/enums.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # --------------------------------
 #             Subd Enum 
 # This shell aims to automate 
@@ -11,5 +13,20 @@ Example: ./enums.sh example.com
 Note: nice to have some tools:
       - amass
       - subfinder
-      - assetfinder
 "
+
+# verify args
+if [ $# -ne 1 ];
+then
+    echo "$USAGE"
+    exit 1
+fi
+DOMAIN="$1"
+OUTDIR="enum_output_$(DOMAIN)"
+mkdir -p "$OUTDIR"
+
+# verify if exists
+AMASS=/snap/bin/amass
+SUBFINDER=/usr/local/bin/subfinder
+
+if [ -x ]
