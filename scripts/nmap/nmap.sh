2d1eac96ffc165108015b9b909dd109cf328267f
raelaraujo0 <raellyedson1@gmail.com>
2025-10-28 09:48:37 -0300

    feat: add scan mode decision in argv

--- scripts/nmap/nmap.sh
+++ scripts/nmap/nmap.sh
@@ -8,19 +8,26 @@ set -euo pipefail
 #       some scans process
 # --------------------------------
 
-USAGE="Usage: ./nmap.sh <target/example>
-Example: ./nmap.sh example
+USAGE="Usage: ./nmap.sh <target> <Scan Mode>
+
+A: Agressive Scan
+S: Stealth scan
+
+Example: ./nmap.sh example.com A
+
 Note: run chmod +x nmap.sh before exec
 "
 
 # verify args
-if [ $# -ne 1 ]; 
+if [ $# -ne 2 ]; 
 then
   echo "$USAGE"
   exit 1
 fi
 
 TARGET="$1"
+OUTDIR="nmap_output_$TARGET"
+mkdir -p "$OUTDIR"
 
 # verify if exists
 NMAP=/usr/bin/nmap
@@ -28,12 +35,6 @@ NMAP=/usr/bin/nmap
 if [ -x "$NMAP" ];
 then
     echo "Nmap exists at $NMAP"
-
-    # -sS : half scan, doesnt complete 3-w-h-s
-    # -Pn : assumes hosts is on
-    # -n : never do DNS resolution
-    
-    nmap -sS -Pn -n $TARGET
 
 else
     echo "Nmap NOT found in /usr/bin/nmap. Installing"
@@ -47,4 +48,13 @@ else
         echo "Error: nmap still isnt available"
         exit 1
     fi
+fi
+
+if [ "$2" == "A" ];
+then
+    echo "Startin Agressive scan on $TARGET"
+    nmap -sV -sC -T4 $TARGET > $OUTDIR/nmap_agressive_$TARGET.txt
+else 
+    echo "Startin Stealth scan on $TARGET"
+    nmap -sS -sV -T1 --scan-delay 250ms $TARGET > $OUTDIR/nmap_stealth_$TARGET.txt
 fi
