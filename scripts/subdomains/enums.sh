8e51050bbd50c38e9b9031b3ae284698b568df76
raelaraujo0 <raellyedson1@gmail.com>
2025-10-28 08:54:41 -0300

    feat: add verify, after scan create target directory and put txt scan file inside

--- scripts/subdomains/enums.sh
+++ scripts/subdomains/enums.sh
@@ -21,12 +21,41 @@ then
     echo "$USAGE"
     exit 1
 fi
+
 DOMAIN="$1"
-OUTDIR="enum_output_$(DOMAIN)"
-mkdir -p "$OUTDIR"
+OUTDIR="enum_output_$DOMAIN"
+mkdir -p "$OUTDIR" #-p parents
 
 # verify if exists
 AMASS=/snap/bin/amass
 SUBFINDER=/usr/local/bin/subfinder
 
-if [ -x ]
+# verify AMASS ENUM
+if [ -x "$AMASS" ]; 
+then
+    echo "Amass found at $AMASS"
+else
+    echo "Amass not found in /usr/bin/amass. Installing"
+    # some distros doesnt uses snap
+    echo "= = INSTALLING THROUGHT SNAP= ="
+    sudo apt update && sudo snap install amass enum -y
+fi
+
+# verify SUBFINDER
+if [ -x "$SUBFINDER" ]; then
+    echo "Subfinder exists at $SUBFINDER"
+
+    subfinder -d $DOMAIN > "$OUTDIR/subfinder_$DOMAIN.txt"
+
+else
+    echo "Subfinder not found in /usr/local/bin/subfinder"
+    sudo apt update && sudo apt install subfinder
+
+    # verify again
+    if [ -x  "$SUBFINDER"];
+    then
+        echo "Subfinder avilable $SUBFINDER"
+    else
+        echo "Error: Subfinder still isnt available"
+    fi
+fi
