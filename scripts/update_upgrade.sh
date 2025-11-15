abc156cbab9d90f2e13ebb5f50c0adef99d72cff
Rael Araujo <raellyedson1@gmail.com>
2025-11-15 19:30:25 -0300

    add autoclean n autoremove

--- scripts/update_upgrade.sh
+++ scripts/update_upgrade.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 echo "- - - UPDATE - - -"
-
 sudo apt update -y
 
 echo "- - - UPGRADE - - -"
-
 sudo apt upgrade -y
+
+echo "- - - AUTOREMOVE && AUTOCLEAN - - -"
+sudo apt autoremove && sudo apt autoclean
